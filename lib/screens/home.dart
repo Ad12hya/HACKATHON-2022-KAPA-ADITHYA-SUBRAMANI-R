3de477621b99efae59cdb315c7e37a734068c471
Pragadesh BS <pragadeshbs@protonmail.com>
2022-02-15 16:46:13 +0530

    add menu button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,6 @@
+import 'package:ccpc/utils/constants.dart';
+import 'package:ccpc/utils/widget_functions.dart';
+import 'package:ccpc/widgets/box_icon.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,13 +8,33 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('CCPC'),
-      ),
-      body: const Center(
-        child: Text("vitae proin sagittis"),
-      ),
+    final Size size = MediaQuery.of(context).size;
+    const double padding = 25;
+    return SafeArea(
+      child: Scaffold(
+          body: SizedBox(
+        width: size.width,
+        height: size.height,
+        child: Column(
+          children: [
+            addVerticalSpace(padding),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: padding),
+              child: Row(
+                children: const [
+                  BoxIcon(
+                      child: Icon(
+                        Icons.menu,
+                        color: colorBlack,
+                      ),
+                      height: 50,
+                      width: 50)
+                ],
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const colorWhite = Colors.white;
+const colorGrey = Color.fromRGBO(141, 141, 141, 1.0);
+const colorDarkBlue = Color.fromRGBO(20, 25, 45, 1.0);
+const colorBlack = Color.fromRGBO(48, 47, 48, 1.0);
