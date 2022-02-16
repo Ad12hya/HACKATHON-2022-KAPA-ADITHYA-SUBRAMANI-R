469d059eb48e178d094493d74290aaeec2647007
Pragadesh BS <pragadeshbs@protonmail.com>
2022-02-16 18:45:52 +0530

    add title, modified readme

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,28 +10,39 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     const double padding = 25;
+    const sidePadding = EdgeInsets.symmetric(horizontal: padding);
+    final ThemeData themeData = Theme.of(context);
     return SafeArea(
       child: Scaffold(
           body: SizedBox(
         width: size.width,
         height: size.height,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             addVerticalSpace(padding),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: padding),
+              padding: sidePadding,
               child: Row(
                 children: const [
                   BoxIcon(
                       child: Icon(
-                        Icons.menu,
+                        Icons.notes_rounded,
                         color: colorBlack,
                       ),
                       height: 50,
                       width: 50)
                 ],
               ),
-            )
+            ),
+            addVerticalSpace(padding),
+            Padding(
+              padding: sidePadding,
+              child: Text(
+                "CCPC",
+                style: themeData.textTheme.headline1,
+              ),
+            ),
           ],
         ),
       )),
